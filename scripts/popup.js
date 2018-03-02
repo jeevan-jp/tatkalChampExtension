47a17f7ff80d8a51a675b31c9fee914475c2e8a4
jeevan-jp <jiwanprakash65@gmail.com>
2018-03-02 22:05:10 +0530

    Added ticket preview details

--- scripts/popup.js
+++ scripts/popup.js
@@ -5,3 +5,24 @@ $("#createForm").on('click', () => {
 $("#bookNow").on('click', () => {
 	window.open("https://www.irctc.co.in/eticketing/loginHome.jsf", '_blank');
 });
+
+var storedData;
+
+chrome.storage.sync.get('poorijaankari1', (data) => {
+	storedData = JSON.parse(data['poorijaankari1']);
+	var count = 0;
+	for(var i=1; i<5; i++) {
+		if(storedData['p' + i + 'Name'])
+			count++;
+	}
+	console.log(storedData);
+	if(storedData) {
+		$('#preview').css('display', 'block');
+		$('#fromStation').html(storedData['jpform_fromStation']);
+		$('#toStation').html(storedData['jpform_toStation']);
+		$('#date').html('Date: ' + storedData['jpform:journeyDateInputDate']);
+		$('#train').html(storedData['trainNo']);
+		$('#class').html('Selected Class: ' + storedData['selectedClass']);
+		$('#count').html('No. of tickets: ' + count)
+	}
+});
